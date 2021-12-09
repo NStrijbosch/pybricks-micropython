ef6a9f5d366b98733b90c19efeb39a1cad37f304
NStrijbosch <n.strijbosch@gmail.com>
2021-12-09 13:58:18 +0100

    small fix

--- tests/pup/ble/ble1_prime.py
+++ tests/pup/ble/ble1_prime.py
@@ -6,28 +6,35 @@ Hardware Module: Technic Hub (ble1_technic.py) and Prime Hub (ble1_prime.py)
 Description: Tests broadcast receive and transmit.
 """
 
-from pybricks.hubs import ThisHub
+from pybricks.hubs import PrimeHub
 from pybricks.parameters import Color
 from pybricks.tools import wait
 from pybricks.ble import Broadcast
 
 
-hub = ThisHub()
+hub = PrimeHub()
 hub.light.on(Color.WHITE)
 
 # Initialize broadcast with two topics.
 radio = Broadcast(topics=["counter", "hue"])
 
-for count in range(100):
+while True:
 
     # Read the counter value.
     counter_data = radio.receive_bytes("counter")
 
+    # If we received it, display it, and send a response.
+    if counter_data:
+        count = counter_data[0]
+        hub.display.number(count)
 
+        # Send back a hue value.
+        hue_data = int(count * 3.6).to_bytes(2, "little")
+        radio.send_bytes("hue", hue_data)
 
-    # Send back a hue value.
-    hue_data = int(count * 3.6).to_bytes(2, "little")
-    radio.send_bytes("hue", hue_data)
+        # Stop if we reach the end.
+        if count >= 99:
+            break
 
     # Don't need to update all the time.
-    wait(100)
+    wait(10)
